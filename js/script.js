88e6670d838482ecd9bb1f5963de29dd6a3503f6
Annetkhavere <anneywan5@gmail.com>
2022-03-20 09:03:32 +0300

    add an  addd order button

--- js/script.js
+++ js/script.js
@@ -1,58 +1,138 @@
 $(document).ready(function() {
-
-//placing an order
   $("#ordering").submit(function(event) {
-    event.preventDefault();
-    placeOrder();
+      event.preventDefault();
+      placeOrder();
   });
 
-  function placeOrder() {
-    //getting inputs from the form
-    let name = $("input#name").val();
-    let size = $("#pizza-size").val();
-    let crust = $("#crust-type").val();
-    let toppings = $("#topping").val();
-    let number = $("input#pizza-number").val();
-
-    //prizes of crust according to size
-    let crustCostAccordingToSize;
-    if (crust === "Crispy" || crust === "Stuffed") {
-      if (size === "Small") {
-        crustCostAccordingToSize = 500;
-      } else if (size === "Medium") {
-        crustCostAccordingToSize = 800;
-      } else if(size === "Large") {
-        crustCostAccordingToSize = 1200;
-      }
-    } else if(crust === "Romana" || crust === "Grilled") {
-      if (size === "Small") {
-        crustCostAccordingToSize = 450;
-      } else if(size === "Medium") {
-        crustCostAccordingToSize = 750;
-      } else if (size === "Large") {
-        crustCostAccordingToSize = 1050;
-      }
-    } else if (crust === "Flatbread" || crust === "Pan-pizza" || crust === "Gluten-Free") {
-      if (size === "Small") {
-        crustCostAccordingToSize = 600;
-      } else if(size === "Medium") {
-        crustCostAccordingToSize = 850;
-      } else if(size === "Large") {
-        crustCostAccordingToSize = 1250;
-      }
+  function placeOrder() { //function for placing an order
+    //get inputs from form
+    var name = $("input#name").val();
+    var size = $("#pizza-size").val();
+    var crust = $("#crust-type").val();
+    var toppings = [];
+    $.each($('input[name="topping"]:checked'),
+        function() {
+            toppings.push($(this).val());
+        });
+  var number = parseInt($("#pizza-number").val());
+    var pizzaSizeCost; //set different prices for the different pizza flavors depending on their size
+    if (size === "Small") {
+          pizzaSizeCost = 300;
+           } else if(size === "Medium") {
+         pizzaSizeCost = 400;
+           } else if(size === "Large") {
+          pizzaSizeCost = 500;
+          }
+    var crustCost; //set prices for different crust types
+       if (crust === "Crispy") {
+     crustCost = 200;
+    } else if (crust === "Stuffed") {
+       crustCost = 220;
+    } else if(crust === "Romana") {
+        crustCost = 230;
+    } else if (crust === "Grilled") {
+        crustCost = 250;
+    } else if (crust === "Flatbread") {
+       crustCost = 200;
+    } else if (crust === "Pan-pizza") {
+        crustCost = 250;
+    } else if (crust === "Gluten-Free") {
+       crustCost = 300;
     }
-  //prizes of toppings according to size
-let toppingCost;
-if (toppings === "Broccoli" || toppings === "Eggplant") {
-  toppingCost = 200;
-} else if(toppings === "Sausage" || toppings === "Mushroom") {
-  toppingCost = 300;
-} else if (toppings === "Capers" || toppings === "Radicchio") {
-  toppingCost = 250;
-}
-let amount = (crustCostAccordingToSize + toppingCost);
-let totalAmount = parseInt(amount * number);
-alert(name + " Your total price is " + totalAmount);
-}
-
+    var checkboxes = $('input[name="topping"]:checked').length; //get number of checkboxes checked
+    if (checkboxes <= 3) { //limit number of checkboxes allowed to not more than 3
+        if (size === "Small") {
+            var toppingsCost = checkboxes * 70;
+        } else if (size === "Medium") {
+            var toppingsCost = checkboxes * 100;
+        } else if (size === "Large") {
+            var toppingsCost = checkboxes * 130;
+        }
+        $("input[type='checkbox']:not(:checked)").prop({ //disable unchecked boxes
+            disabled: true
+        });
+        $('#placeorder').prop('disabled', true); //deactivate button after order is made to prevent client from changing order once the order is placed
+        var price = parseInt((pizzaSizeCost + crustCost + toppingsCost));
+        var totalPrice = parseInt(price * number);
+        alert(totalPrice);
+    } 
+    
+  }
 });
+
+
+
+
+
+
+
+// $(document).ready(function() {
+
+// //placing an order
+//   $("#ordering").submit(function(event) {
+//     event.preventDefault();
+//     placeOrder();
+//   });
+
+//   function placeOrder() {
+//     //getting inputs from the form
+//     var name = $("input#name").val();
+//     var size = $("#pizza-size").val();
+//     var crust = $("#crust-type").val();
+//     var toppings = []; // define toppings as an empty array
+//     $.each($("input[name='topping']:checked"),
+//     function () {
+//       toppings.push($(this).val()); //for each input whose name is topping, when checked, we add it to the empty array
+//     }); 
+//     var number = parseInt($("#pizza-number").val());
+
+//     //prizes according to the Pizza Size
+    
+//     if (size === "Small") {
+//       var pizzaSizeCost = 300;
+//     } else if(size === "Medium") {
+//        var pizzaSizeCost = 400;
+//     } else if(size === "Large") {
+//        var pizzaSizeCost = 500;
+//     }
+
+//     //prizes of crust according to size
+    
+//     if (crust === "Crispy") {
+//       var crustCost = 200;
+//     } else if (crust === "Stuffed") {
+//       var crustCost = 220;
+//     } else if(crust === "Romana") {
+//        var crustCost = 230;
+//     } else if (crust === "Grilled") {
+//        var crustCost = 250;
+//     } else if (crust === "Flatbread") {
+//       var crustCost = 200;
+//     } else if (crust === "Pan-pizza") {
+//        var crustCost = 250;
+//     } else if (crust === "Gluten-Free") {
+//        var crustCost = 300;
+//     }
+//   //prizes of toppings according to size
+//   var checkboxes = $('input[name="topping"]:checked').length;
+//   if (checkboxes <= 3) {
+//     if(size === "Small") {
+//       var toppingsCost = checkboxes * 150;
+//     } else if (size === "Medium") {
+//       var toppingsCost = checkboxes * 200;
+//     } else if(size === "Large") {
+//       var toppingsCost = checkboxes * 250;
+//     }
+
+//     $("input[type='checkbox']:not(:checked)").prop({
+//       disabled: true  //disable unchecked boxes
+//     });
+//     $("#place-order").prop("disabled", true); //deactvate button after an order is made
+// var amount = pizzaSizeCost + crustCost + toppingsCost;
+// var totalAmount = parseInt(amount * number);
+// console.log(totalAmount);
+//   }
+
+// }
+
+// });
